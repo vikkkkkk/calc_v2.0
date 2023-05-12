78515c131791d2bcfec3232635687eb78988dc4d
Hsiu Ayesha <hsiuayes@un-m3.kzn.21-school.ru>
2023-05-12 18:56:15 +0300

    comment for .cpp

--- src/model/model.cpp
+++ src/model/model.cpp
@@ -61,6 +61,88 @@ void Model::GetSymbol() {
   }
 }
 
+std::string Model::Parser() {
+  std::string newStr; // создание пустой строки newStr
+  bool check = false; // создание переменной check типом bool со значением false
+  for (size_t i = 0; i < p_stream_string.size(); i++) {
+    // начало цикла перебора всех символов строки p_stream_string, начиная с индекса 0 и заканчивая последним индексом строки
+    char c = p_stream_string[i]; // создание переменной c типом char и присвоение её значения символа, 
+    // соответствующего текущему индексу i в строке p_stream_string
+    switch (c) { // начало конструкции switch-case для проверки текущего символа.
+      case 'm': // если текущий символ - 'm', то выполняется следующий код
+        if (p_stream_string.substr(i, 3) == "mod") { // проверяем, совпадает ли следующая подстрока в `p_stream_string` со строкой "mod"
+          newStr += "mod"; // если подстрока равна "mod", добавляем эту строку в переменную `newStr`
+          i += 2;
+          // увеличиваем значение `i` на 2, так как мы добавили в `newStr` три символа: "m", "o" и "d", а в следующей итерации цикла `i` увеличится еще на 1
+        } else {
+          check = true;
+          // если подстрока не равна "mod", устанавливаем переменную `check` в значение `true`
+        }
+        break;
+      case 's':
+        if (p_stream_string.substr(i, 3) == "sin") {
+          newStr += "sin";
+          i += 2;
+        } else if (p_stream_string.substr(i, 4) == "sqrt") {
+          newStr += "sqrt";
+          i += 3;
+        } else {
+          check = true;
+        }
+        break;
+      case 'c':
+        if (p_stream_string.substr(i, 3) == "cos") {
+          newStr += "cos";
+          i += 2;
+        } else {
+          check = true;
+        }
+        break;
+      case 't':
+        if (p_stream_string.substr(i, 3) == "tan") {
+          newStr += "tan";
+          i += 2;
+        } else {
+          check = true;
+        }
+        break;
+      case 'a':
+        if (p_stream_string.substr(i, 4) == "atan") {
+          newStr += "atan";
+          i += 3;
+        } else if (p_stream_string.substr(i, 4) == "asin") {
+          newStr += "asin";
+          i += 3;
+        } else if (p_stream_string.substr(i, 4) == "acos") {
+          newStr += "acos";
+          i += 3;
+        } else {
+          check = true;
+        }
+        break;
+      case 'l':
+        if (p_stream_string.substr(i, 2) == "ln") {
+          newStr += "ln";
+          i += 1;
+        } else if (p_stream_string.substr(i, 3) == "log") {
+          newStr += "log";
+          i += 2;
+        } else {
+          check = true;
+        }
+        break;
+      default:
+        newStr += c;
+        break;
+    }
+  }
+  if (check) {
+    throw std::invalid_argument("AHTUNG: invalid symbol");
+    // проверяем, если значение переменной `check` равно `true`, выбрасываем исключение
+  }
+  return newStr;
+}
+
 }  // namespace s21
 
 
