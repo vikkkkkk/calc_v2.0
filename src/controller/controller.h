76bf330a656f7ae464132d15e3d41fba9a4040c5
Hsiu Ayesha <hsiuayes@un-m3.kzn.21-school.ru>
2023-05-13 18:16:40 +0300

    lol

--- src/controller/controller.h
+++ src/controller/controller.h
@@ -6,9 +6,24 @@
 #include "../model/model.h"
 
 namespace s21 {
+class Controller {
+private:
+std::string mInputString;
+public:
+Controller(std::string const &str = "") : mInputString(str) {}
 
+std::pair<std::string, double> calculation();
 
+std::vector<double> CrCalc(double loanAmount, int term, 
+                           double interestRate,  bool paymentType);
 
+std::vector<double> DepCalc(bool check, double deposit_term, 
+                            double deposit_amount, double interest_rate, 
+                            double tax_rate, double replenishments_month, 
+                            double replenishments_sum, 
+                            double partial_withdrawals_month, 
+                            double partial_withdrawals_sum); 
+};
 }  // namespace s21
 
 #endif  // SRC_CONTROLLER_CONTROLLER_H
