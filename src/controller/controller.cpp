afb2ece2cb413d593cb55c636b848544574cb577
Hsiu Ayesha <hsiuayes@un-m3.kzn.21-school.ru>
2023-05-13 18:18:13 +0300

    lol2

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -1,6 +1,25 @@
 #include "controller.h"
 
 namespace s21 {
+std::pair<std::string, double> Controller::calculation() {
+  std::string error = "";
+  double res = 0.0;
+  try {
+    Model model(mInputString);
+    res = model.Calc();
+  } catch (const std::exception& e) {
+    error = e.what();
+  }
+  
+  std::pair<std::string, double> result = {error, res};
+  return result;
+}
 
+std::vector<double> Controller::CrCalc(double loanAmount, int term,
+                                       double interestRate,
+                                       bool paymentType) {
+  Model model(mInputString);
+  return model.CreditCalc(loanAmount, term, interestRate, paymentType);
+}
 
 }  // namespace s21
